bcfb898e138e180a69b8cbe1b26ee53859344873
eliezerKott <k4131755@gmail.com>
2022-06-28 16:01:33 +0300

    final set camera face and assesment test with instr

--- src/pages-order/firstAssessement/AssessementTest/index.jsx
+++ src/pages-order/firstAssessement/AssessementTest/index.jsx
@@ -4,6 +4,7 @@ import img from './man.svg'
 import img1 from './Vector.svg'
 // Creator : Team2 - eliezer kott
 // need to add header
+// props of icon title text and function for in click 
 function FirstAssement(){
     return <div>
         <img className={styles.manSitting} src={img} alt="manSittiyng" />
